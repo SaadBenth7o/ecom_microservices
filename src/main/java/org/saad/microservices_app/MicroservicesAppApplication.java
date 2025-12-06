2a5a4f93bdbc4a88236e750be4b75239b22dea63
SaadBenth7o <bendahou.saad@gmail.com>
2025-12-06 09:07:11 +0100

    first commit

---
+++ src/main/java/org/saad/microservices_app/MicroservicesAppApplication.java
@@ -0,0 +1,13 @@
+package org.saad.microservices_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesAppApplication.class, args);
+    }
+
+}
